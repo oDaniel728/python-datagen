58d4d4d1acc727b6f205b4d5ae00ee2df9513452
odaniel <danielmenezes0822@gmail.com>
2026-06-01 15:40:56 -0400

    Add max_level parameter to Enchantment constructor for enhanced functionality

--- datagen/utils/repr/enchantment.py
+++ datagen/utils/repr/enchantment.py
@@ -2,8 +2,9 @@ from datagen.utils.minecraft.identifier import Identifier
 
 
 class Enchantment():
-    def __init__(self, id: Identifier):
+    def __init__(self, id: Identifier, max_level: int = -1):
         self.id = id
+        self.max_level = max_level
 
     def __str__(self) -> str:
         return str(self.id)
