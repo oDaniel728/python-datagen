5420487a6d5bffc225951a744078ac2dc8b9127b
odaniel <danielmenezes0822@gmail.com>
2026-06-01 13:48:41 -0400

    Add AnonymousFunction class for dynamic function creation in datapacks

---
+++ datagen/function/anonymousfunction.py
@@ -0,0 +1,15 @@
+from uuid import uuid4
+
+from datagen.datapack.datapack import DataPack
+from datagen.datapack.namespace import Namespace
+from datagen.function.function import Function
+from datagen.utils.minecraft.identifier import Identifier
+
+
+class AnonymousFunction(Function):
+
+    def __init__(self, datapack: DataPack):
+        super().__init__(Namespace.temp.identifier(f"fun{len(Namespace.temp.functions)}"))
+        self.datapack = datapack
+        self.datapack.add_namespace(Namespace.temp)
+        Namespace.temp.add(self)
