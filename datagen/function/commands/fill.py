e6bad3c8c7c492581796acc79db87170e92f32f1
odaniel <danielmenezes0822@gmail.com>
2026-05-31 22:32:09 -0400

    Refactor Fill class to simplify constructor and remove unused NBT handling

--- datagen/function/commands/fill.py
+++ datagen/function/commands/fill.py
@@ -1,19 +1,16 @@
 from datagen.function.commands.command import Command
 from datagen.types.protocols.todict import ToDict
-from datagen.utils.minecraft.BlockPosition import BlockPosition
+from datagen.utils.minecraft.blockposition import BlockPosition
 from datagen.utils.minecraft.identifier import Identifier
+from datagen.utils.repr.block import Block
 from datagen.utils.snbtserializer import SNBTSerializer
 
 
 class Fill(Command):
-    def __init__(self, block_pos1: BlockPosition, block_pos2: BlockPosition, block: Identifier, nbt: ToDict | dict):
+    def __init__(self, block_pos1: BlockPosition, block_pos2: BlockPosition, block: Block):
         self.block_pos1 = block_pos1
         self.block_pos2 = block_pos2
         self.block = block
-        self.nbt = nbt if isinstance(nbt, dict) else nbt.to_dict()
-
-    def _nbt_to_string(self) -> str:
-        return SNBTSerializer.serialize(self.nbt)
 
     def to_string(self) -> str:
-        return self.auto_macro(f"fill {self.block_pos1.to_string()} {self.block_pos2.to_string()} {self.block} {self._nbt_to_string()}")
+        return self.auto_macro(f"fill {self.block_pos1.to_string()} {self.block_pos2.to_string()} {self.block}")
