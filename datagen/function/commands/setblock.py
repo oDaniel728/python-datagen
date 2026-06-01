3872a332d9a6b775d60c955dfb9a74a2744eac44
odaniel <danielmenezes0822@gmail.com>
2026-05-31 22:31:47 -0400

    Refactor SetBlock class to use Block type and simplify to_string method

--- datagen/function/commands/setblock.py
+++ datagen/function/commands/setblock.py
@@ -1,18 +1,16 @@
 from datagen.function.commands.command import Command
 from datagen.types.protocols.todict import ToDict
-from datagen.utils.minecraft.BlockPosition import BlockPosition
+from datagen.utils.minecraft.blockposition import BlockPosition
 from datagen.utils.minecraft.identifier import Identifier
+from datagen.utils.repr.block import Block
 from datagen.utils.snbtserializer import SNBTSerializer
 
 
 class SetBlock(Command):
-    def __init__(self, block_pos: BlockPosition, block: Identifier, nbt: ToDict | dict):
+    def __init__(self, block_pos: BlockPosition, block: Block, nbt: ToDict | dict):
         self.block_pos = block_pos
         self.block = block
         self.nbt = nbt if isinstance(nbt, dict) else nbt.to_dict()
 
-    def _nbt_to_string(self) -> str:
-        return SNBTSerializer.serialize(self.nbt)
-
     def to_string(self) -> str:
-        return self.auto_macro(f"setblock {self.block_pos.to_string()} {self.block} {self._nbt_to_string()}")
+        return self.auto_macro(f"setblock {self.block_pos.to_string()} {self.block}")
