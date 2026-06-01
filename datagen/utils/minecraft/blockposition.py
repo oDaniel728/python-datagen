dcc7996eae19425c10c8fe1bca6049949b24232b
odaniel <danielmenezes0822@gmail.com>
2026-05-31 22:31:06 -0400

    Refactor BlockPosition class to inherit from Position3 for improved structure

---
+++ datagen/utils/minecraft/blockposition.py
@@ -0,0 +1,11 @@
+from typing import TYPE_CHECKING
+
+from datagen.utils.repr.position3 import Position3
+
+
+if TYPE_CHECKING:
+    from datagen.utils.minecraft.PlayerPosition import PlayerPosition
+
+class BlockPosition(Position3[int]):
+    def __init__(self, x: int, y: int, z: int):
+        super().__init__(x, y, z)
