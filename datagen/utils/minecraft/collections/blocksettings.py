c7e912b0c0f9d43563deb6e16a2eb7cea612cc50
odaniel <danielmenezes0822@gmail.com>
2026-06-01 12:53:51 -0400

    Refactor LOGS class to inherit from Block.Settings for improved structure

--- datagen/utils/minecraft/collections/blocksettings.py
+++ datagen/utils/minecraft/collections/blocksettings.py
@@ -1,10 +1,11 @@
 from typing import Literal
 
 from datagen.types.protocols.todict import ToDict
+from datagen.utils.repr.block import Block
 
 
 class BlockSettings():
-    class LOGS(ToDict):
+    class LOGS(Block.Settings):
         TAxis = Literal["x", "y", "z"]
         def __init__(self, axis: TAxis):
             self.axis = axis
