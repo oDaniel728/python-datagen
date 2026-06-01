cc7b716b99b78266d40c7bd4284ad97398f2557d
odaniel <danielmenezes0822@gmail.com>
2026-05-31 22:43:10 -0400

    Remove type hint for nbt parameter in SetBlock constructor and simplify its assignment

--- datagen/function/commands/setblock.py
+++ datagen/function/commands/setblock.py
@@ -7,10 +7,9 @@ from datagen.utils.snbtserializer import SNBTSerializer
 
 
 class SetBlock(Command):
-    def __init__(self, block_pos: BlockPosition, block: Block, nbt: ToDict | dict):
+    def __init__(self, block_pos: BlockPosition, block: Block, nbt):
         self.block_pos = block_pos
         self.block = block
-        self.nbt = nbt if isinstance(nbt, dict) else nbt.to_dict()
 
     def to_string(self) -> str:
         return self.auto_macro(f"setblock {self.block_pos.to_string()} {self.block}")
