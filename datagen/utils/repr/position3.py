9f65d96a4a1dcb3e6c9cba286a013910fae177c9
odaniel <danielmenezes0822@gmail.com>
2026-05-31 22:31:14 -0400

    Add conversion methods to Position3 class for enhanced data representation

--- datagen/utils/repr/position3.py
+++ datagen/utils/repr/position3.py
@@ -6,4 +6,16 @@ class Position3[N: int | float]():
 
     def __str__(self) -> str:
         return f"{self.x} {self.y} {self.z}"
+
+    def to_string(self) -> str:
+        return f"{self.x} {self.y} {self.z}"
+    
+    def to_dict(self) -> dict:
+        return {"x": self.x, "y": self.y, "z": self.z}
+    
+    def to_list(self) -> list[N]:
+        return [self.x, self.y, self.z]
+    
+    def to_tuple(self) -> tuple[N, N, N]:
+        return (self.x, self.y, self.z)
         
