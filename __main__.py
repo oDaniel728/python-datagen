fea06fa5d157cf58660c4b85a889a9f1656a75ce
odaniel <danielmenezes0822@gmail.com>
2026-06-02 19:50:28 -0400

    Enhance main script to handle KeyboardInterrupt and add file watching capability

--- __main__.py
+++ __main__.py
@@ -1,4 +1,12 @@
+from sys import argv
+
+from datagen.utils.filewatcher import FileWatcher
 import src.main
 
 if __name__ == "__main__":
-    src.main.main()
+    try:
+        src.main.main()
+        if "--watch" in argv or "-w" in argv:
+            FileWatcher(src.main.__file__).watch(lambda _: src.main.main())
+    except KeyboardInterrupt:
+        print("Exiting...")
