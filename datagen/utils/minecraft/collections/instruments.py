9fc88bcffa92be7f75bb8df41be9aad0a85efa22
odaniel <danielmenezes0822@gmail.com>
2026-06-01 22:49:07 -0400

    data update

---
+++ datagen/utils/minecraft/collections/instruments.py
@@ -0,0 +1,12 @@
+from datagen.utils.minecraft.identifier import Identifier
+from datagen.utils.repr.instrument import Instrument
+
+class Instruments():
+    SEEK_GOAT_HORN = Instrument(Identifier.of('minecraft:seek_goat_horn'))
+    SING_GOAT_HORN = Instrument(Identifier.of('minecraft:sing_goat_horn'))
+    CALL_GOAT_HORN = Instrument(Identifier.of('minecraft:call_goat_horn'))
+    FEEL_GOAT_HORN = Instrument(Identifier.of('minecraft:feel_goat_horn'))
+    PONDER_GOAT_HORN = Instrument(Identifier.of('minecraft:ponder_goat_horn'))
+    ADMIRE_GOAT_HORN = Instrument(Identifier.of('minecraft:admire_goat_horn'))
+    DREAM_GOAT_HORN = Instrument(Identifier.of('minecraft:dream_goat_horn'))
+    YEARN_GOAT_HORN = Instrument(Identifier.of('minecraft:yearn_goat_horn'))
