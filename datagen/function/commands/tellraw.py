9fc492cac051b1558a27eba5fb511cc77cfd1dc8
odaniel <danielmenezes0822@gmail.com>
2026-06-02 15:20:05 -0400

    Add TellRaw command class for sending formatted messages to targets

---
+++ datagen/function/commands/tellraw.py
@@ -0,0 +1,11 @@
+from datagen.function.commands.command import Command
+from datagen.utils.minecraft.targetselector import TargetSelector
+from datagen.utils.minecraft.text import Text
+
+class TellRaw(Command):
+    def __init__(self, target: TargetSelector, message: Text.BaseText):
+        self.target = target
+        self.message = message
+
+    def to_string(self) -> str:
+        return f'tellraw {self.target.to_string()} {self.message.to_string()}'
