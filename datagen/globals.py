b7d7319d3210652827fabf12f3ed3e24680cc02a
odaniel <danielmenezes0822@gmail.com>
2026-06-01 22:48:24 -0400

    Add DatagenConfig class for configuration management and initialization

--- datagen/globals.py
+++ datagen/globals.py
@@ -1,2 +1,27 @@
+import json
+from typing import TypedDict
+
+
 FUNCTIONS_PATH = "function/"
 TAGS_PATH = "tags/"
+CONFIG_PATH = ".datagenconfig"
+
+class DatagenConfig():
+    class TDatagenConfigTBuilderOptions(TypedDict):
+        source: str
+        output: str
+    class TDatagenConfig(TypedDict):
+        builderSettings: DatagenConfig.TDatagenConfigTBuilderOptions
+        
+    config: TDatagenConfig
+
+    __registered = False
+    
+    @staticmethod
+    def register():
+        if DatagenConfig.__registered: return
+        DatagenConfig.__registered = True
+
+        DatagenConfig.config = json.load(open(CONFIG_PATH, "r"))
+
+DatagenConfig.register()
