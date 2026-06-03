02737eacc65cc5e897c708d06b17c37a839782fa
odaniel <danielmenezes0822@gmail.com>
2026-06-02 21:43:27 -0400

    Add CustomCommand class for flexible command handling

---
+++ datagen/function/commands/customcommand.py
@@ -0,0 +1,22 @@
+from datagen.function.commands.command import Command
+
+
+class CustomCommand(Command):
+    def __init__(self, *command: str) -> None:
+        self.command = " ".join(command)
+
+    def to_string(self) -> str:
+        return self.command
+    
+    def __add__(self, other: "Command | str") -> "CustomCommand":
+        if isinstance(other, str):
+            return CustomCommand(self.command + " " + other)
+        else:
+            return CustomCommand(self.command + " " + other.to_string())
+        
+    def __iadd__(self, other: "Command | str") -> "CustomCommand":
+        if isinstance(other, str):
+            self.command += " " + other
+        else:
+            self.command += " " + other.to_string()
+        return self
