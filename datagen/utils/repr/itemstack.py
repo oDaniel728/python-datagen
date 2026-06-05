97ae91864a0e00dbe05574ed424e50011d087197
odaniel <danielmenezes0822@gmail.com>
2026-06-05 14:07:24 -0400

    Refactor ItemStack class to support generic item types

--- datagen/utils/repr/itemstack.py
+++ datagen/utils/repr/itemstack.py
@@ -4,8 +4,8 @@ from datagen.utils.repr.item import Item
 from datagen.utils.repr.position3 import Position3
 
 
-class ItemStack():
-    def __init__(self, item: Item, count: int):
+class ItemStack[I: Item = Item]():
+    def __init__(self, item: I, count: int):
         self.item = item
         self.count = count
 
