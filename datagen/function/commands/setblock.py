9b5fcf7c84a73d674419113f47e0e9a32aac3c91
odaniel <danielmenezes0822@gmail.com>
2026-06-08 21:48:31 -0400

    Refactor SetBlock class: implement __get_str_block method for improved block representation

--- datagen/function/commands/setblock.py
+++ datagen/function/commands/setblock.py
@@ -12,5 +12,22 @@ class SetBlock(Command):
         self.block_pos = block_pos
         self.block = block
 
+    def __get_str_block(self) -> str:
+        # <identifier>[<blockstate>]{<nbt>}
+        state = list[str]()
+        for k, v in self.block.nbt.get_block_state().items():
+            state.append(f"\"{k}\"=\"{v}\"")
+        state_str = ",".join(state)
+        nbt = list[str]()
+        for k, v in self.block.nbt.get_block_entity_data().items():
+            nbt.append(f"\"{k}\":{SNBTSerializer.serialize(v)}")
+        nbt_str = ",".join(nbt)
+
+        out = str()
+        out += ~ self.block.id
+        out += f"[{state_str}]" if state_str else ""
+        out += f"{{{nbt_str}}}" if nbt_str else ""
+        return out
+
     def to_string(self) -> str:
-        return self.auto_macro(f"setblock {self.block_pos} {self.block}")
+        return self.auto_macro(f"setblock {self.block_pos} {self.__get_str_block()}".strip())
