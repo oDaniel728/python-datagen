8582a37f84f365e6d0a6068455dfe3fe439e0bcd
odaniel <danielmenezes0822@gmail.com>
2026-06-14 22:00:31 -0400

    feat: add code snippets for datapack, function, and namespace creation

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
+from datagen.advancement.advancement import Advancement
+from datagen.advancement.advancementbuilder import AdvancementBuilder
+from datagen.advancement.criteria import Criteria
 from datagen.datapack.datapack import DataPack
 from datagen.datapack.namespace import Namespace
 from datagen.function.commands.say import Say
 from datagen.function.function import Function
+from datagen.utils.minecraft.collections.items import Items
+from datagen.utils.minecraft.text._components import LiteralText
 
 
 def main():
-    dp = DataPack("enchantment_demo", "Demonstrating custom enchantments")
-    ns = Namespace("demo")
+    dp = DataPack("test_pack", "This is a test datapack")
+    
+    ns = Namespace("pack")
     dp += ns
-
-    with ~ Function(ns / "hello") as hello:
-        ~ Say("Hello world!")
+    
+    with ~ Function(ns / "test") as test:
+        ~ Say("Hello, world!")
 
     dp.build()
-
-
-main()
-
 #nd
