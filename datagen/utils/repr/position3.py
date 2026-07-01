e8cde9199e8c076fd53073969c2b4a592898ad45
odaniel <danielmenezes0822@gmail.com>
2026-07-01 10:47:58 -0400

    fix: Import Type for type annotations in Position3 class methods

--- datagen/utils/repr/position3.py
+++ datagen/utils/repr/position3.py
@@ -1,7 +1,6 @@
-from typing import Any
+from typing import Any, Type
 
 from datagen.function.functionmacroargument import FunctionMacroArgument
-
 
 class Position3[N: int | float | str | FunctionMacroArgument]():
     def __init__(self, x: N, y: N, z: N):
@@ -28,6 +27,9 @@ class Position3[N: int | float | str | FunctionMacroArgument]():
     def get_x(self) -> N: return self.x
     def get_y(self) -> N: return self.y
     def get_z(self) -> N: return self.z
+    def get_x_as[T](self, t: Type[T]) -> T: return self.x # type: ignore
+    def get_y_as[T](self, t: Type[T]) -> T: return self.y # type: ignore
+    def get_z_as[T](self, t: Type[T]) -> T: return self.z # type: ignore
     def set_x(self, x: N) -> None: self.x = x
     def set_y(self, y: N) -> None: self.y = y
     def set_z(self, z: N) -> None: self.z = z
