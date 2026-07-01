f98bd4dcf498478ea585eb34208f0b2d15c27ef6
odaniel <danielmenezes0822@gmail.com>
2026-07-01 10:48:01 -0400

    fix: Update position handling in CommandBlock to use get_y_as and get_z_as methods

--- datagenpp/extras/item/commandblock.py
+++ datagenpp/extras/item/commandblock.py
@@ -1,14 +1,9 @@
-from typing import Callable
 
 from datagen.function.commands.command import Command
-from datagen.utils.minecraft.blockposition import BlockPosition
 from datagen.utils.minecraft.collections.blocks import Blocks
-from datagen.utils.minecraft.identifier import Identifier
 from datagen.utils.minecraft.relativeblockposition import RelativeBlockPosition
 from datagen.utils.repr.block import Block
 from datagen.utils.repr.blockschematic import BlockSchematic
-from datagen.utils.repr.item import Item
-from datagen.utils.repr.itemstack import ItemStack
 from datagenpp.extras.item.chaincommandblock import ChainCommandBlock
 from datagenpp.extras.item.settings.commandblocksettings import CommandBlockSettings
 
@@ -28,12 +23,12 @@ class CommandBlock(Block[CommandBlockSettings]):
         def move_front():
             nonlocal pos
             match direction:
-                case "down": pos.set_y(pos.get_y() - 1)
-                case "up": pos.set_y(pos.get_y() + 1)
-                case "north": pos.set_z(pos.get_z() - 1)
-                case "south": pos.set_z(pos.get_z() + 1)
-                case "west": pos.set_x(pos.get_x() - 1)
-                case "east": pos.set_x(pos.get_x() + 1)
+                case "down": pos.set_y(pos.get_y_as(int) - 1)
+                case "up": pos.set_y(pos.get_y_as(int) + 1)
+                case "north": pos.set_z(pos.get_z_as(int) - 1)
+                case "south": pos.set_z(pos.get_z_as(int) + 1)
+                case "west": pos.set_x(pos.get_x_as(int) - 1)
+                case "east": pos.set_x(pos.get_x_as(int) + 1)
 
         for i, raw in enumerate(_raws):
             _is_first = i == 0
