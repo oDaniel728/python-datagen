3a4aaf307496bdc47b35dd7d1ea06300e024a609
odaniel <danielmenezes0822@gmail.com>
2026-07-03 16:49:39 -0400

    feat: add __str__ method to ParticleType for improved string representation

--- datagen/utils/repr/particle.py
+++ datagen/utils/repr/particle.py
@@ -4,3 +4,6 @@ from datagen.utils.minecraft.identifier import Identifier
 class ParticleType():
     def __init__(self, id: Identifier) -> None:
         self.id = id
+
+    def __str__(self) -> str:
+        return str(self.id)
