d3bafd1bec76584aed44903565eedb10589e3d84
odaniel <danielmenezes0822@gmail.com>
2026-07-06 19:54:47 -0400

    feat: add Self type hint support for method chaining in AppliedStatusEffect

--- datagen/utils/repr/appliedstatuseffect.py
+++ datagen/utils/repr/appliedstatuseffect.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Self
 
 
 if TYPE_CHECKING:
@@ -27,3 +27,23 @@ class AppliedStatusEffect():
         if self.show_icon is not None:
             data["show_icon"] = self.show_icon
         return data
+    
+    def with_duration(self, duration: int) -> "Self":
+        self.duration = duration
+        return self
+    
+    def with_amplifier(self, amplifier: int) -> "Self":
+        self.amplifier = amplifier
+        return self
+    
+    def with_ambient(self, ambient: bool) -> "Self":
+        self.ambient = ambient
+        return self
+    
+    def with_show_particles(self, show_particles: bool) -> "Self":
+        self.show_particles = show_particles
+        return self
+    
+    def with_show_icon(self, show_icon: bool) -> "Self":
+        self.show_icon = show_icon
+        return self
