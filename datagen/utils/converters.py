1d776ef803af925a86e1e7470339b4d7300bcd1f
odaniel <danielmenezes0822@gmail.com>
2026-07-06 09:52:16 -0400

    feat: a new IdentifierConverter

--- datagen/utils/converters.py
+++ datagen/utils/converters.py
@@ -1,5 +1,8 @@
 from typing import Any, Protocol
 
+from datagen.types.util.holder import Holder
+from datagen.utils.minecraft.identifier import Identifier
+
 
 class Dictionary[K, V]():
     class HasToDict(Protocol):
@@ -17,4 +20,23 @@ class Dictionary[K, V]():
             else:
                 return v.dict
         else:
-            raise ValueError(f"Cannot convert {v} to Dictionary")
+            raise ValueError(f"Cannot convert {v}:{type(v)} to Dictionary")
+        
+class IdentifierConverter():
+    @staticmethod
+    def auto(v: Any) -> Identifier:
+        if isinstance(v, dict):
+            if "id" in v:
+                return IdentifierConverter.auto(v["id"])
+            else:
+                raise ValueError("Cannot convert dict to Identifier, missing 'id' key")
+        elif isinstance(v, str):
+            return Identifier.of(v)
+        elif isinstance(v, Identifier):
+            return v
+        elif isinstance(v, (tuple, list)) and len(v) == 2:
+            return Identifier.of(v[0], v[1])
+        elif isinstance(v, Holder):
+            return IdentifierConverter.auto(v.get())
+        else:
+            raise ValueError(f"Cannot convert {v}:{type(v)} to Identifier")
